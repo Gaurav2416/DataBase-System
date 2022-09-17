9ceae41b74240cb1aa766a9c4de1b998c740cb00
Gaurav2416 <gauravtaneja267@gmail.com>
2022-09-16 22:50:27 -0500

    All projects

---
+++ Project2/Part3/Task.sql
@@ -0,0 +1,17 @@
+Insert into staff VALUES('G1','A','T','Lib','12-FEB-2021','')
+
+--Task 2
+Select * from Satff;
+Select * from Inventory;
+Select * from Member;
+Select * from Visitor;
+Select * from Maintains stock;
+Select * from Guest;
+Select * from Employee;
+Select * from BoolIssued;
+
+--Task3
+select * from BoolIssued weeklyReport;
+
+--Task4
+Select * from Visitor v group by v.checkin;
